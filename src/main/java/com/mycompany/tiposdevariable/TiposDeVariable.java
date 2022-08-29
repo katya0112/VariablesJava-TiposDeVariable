e43875c2287ba9698e968558a8d5df99b5445223
katya0112 <hdzkatya07@gmail.com>
2022-08-29 09:26:18 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/tiposdevariable/TiposDeVariable.java
@@ -0,0 +1,17 @@
+// Variables.java
+
+public class TiposDeVariable {
+    public static void main(String[] args) {
+        // declarar la variable speed de tipo int (números enteros)
+        int speed;
+        
+    //Actualizar el contenido de la variable speed:
+    speed = 10; // si ya habias declarado la variable
+    
+    // Declarar una variable y asignarle un valor al mismo tiempo:
+    int salary = 1000;
+    
+    // Crear una variable de tipo string:
+    String eployeeName = "Anahí Salgado";
+    }
+}
